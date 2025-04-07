174e3e8ed2ce61a439c3b7796d7b671022382394
Sergey842248 <fallahchristine09@gmail.com>
2025-04-07 05:36:43 +0200

    updated app version to 2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 2
-        versionName = "2.0.1"
+        versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
