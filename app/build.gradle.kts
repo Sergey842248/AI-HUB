10bfb3732fc34e1307ec6e174286d0322ecddbe3
Sergey842248 <fallahchristine09@gmail.com>
2025-07-06 12:30:19 +0200

    updated App-version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.future.aihub"
         minSdk = 24
         targetSdk = 35
-        versionCode = 30
-        versionName = "3.0"
+        versionCode = 31
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
